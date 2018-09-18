e2cc9b8f3dc341776f031ef6886c9e5387bef9cd
EndriuPC <endriu102.ms@gmail.com>
2018-09-19 00:01:47 +0200

    First commit

---
+++ FlowerStore.WebUI/Global.asax.cs
@@ -0,0 +1,20 @@
+﻿using FlowerStore.WebUI.Infrastructure;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace FlowerStore.WebUI
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory());
+        }
+    }
+}
---
+++ FlowerStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Routing;
+using FlowerStore.Domain.Abstract;
+using FlowerStore.Domain.Entities;
+using Moq;
+using Ninject;
+
+namespace FlowerStore.WebUI.Infrastructure
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+
+        public NinjectControllerFactory()
+        {
+            ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null ? null : (IController)ninjectKernel.Get(controllerType);
+        }
+
+        private void AddBindings()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>
+            {
+                new Product {Name = "Goździk", Price = 2},
+                new Product {Name = "Tulipan", Price = 3},
+                new Product {Name = "Róża", Price = 5}
+            }.AsQueryable());
+
+            ninjectKernel.Bind<IProductRepository>().ToConstant(mock.Object);
+        }
+
+
+    }
+}
