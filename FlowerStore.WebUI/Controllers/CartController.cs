db70a4775824c3398a015cfef500fba76bd8c852
EndriuPC <endriu102.ms@gmail.com>
2018-10-01 23:07:14 +0200

    Added AddToCart button and summary

---
+++ FlowerStore.WebUI/Controllers/CartController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FlowerStore.Domain.Abstract;
+using FlowerStore.Domain.Entities;
+using FlowerStore.WebUI.Models;
+
+namespace FlowerStore.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly IProductRepository _repository;
+
+        public CartController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel {Cart = GetCart(), ReturnUrl = returnUrl});
+        }
+
+        public RedirectToRouteResult AddToCart(int productID, string returnUrl)
+        {
+            Product product = _repository.Products.FirstOrDefault(x => x.ProductID == productID);
+
+            if (product != null)
+            {
+                GetCart().AddItem(product, 1);
+            }
+
+            return RedirectToAction("Index", new {returnUrl});
+        }
+
+        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
+        {
+            Product product = _repository.Products.FirstOrDefault(x => x.ProductID == productId);
+
+            if (product != null)
+            {
+                GetCart().RemoveLine(product);
+            }
+
+            return RedirectToAction("Index", new {returnUrl});
+        }
+
+        private Cart GetCart()
+        {
+            Cart cart = (Cart) Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+
+            return cart;
+        }
+    }
+}
